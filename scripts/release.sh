e4a200365912ef9dce3fe3e9ebf279fa1b6ce98d
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2020-07-07 14:27:59 +0200

    Improve release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" != "develop" ]]; then
+  echo 'Not on branch develop. Aborting.';
+  exit 1;
+fi
+
 git merge master
 git checkout master
 git pull
 git merge develop
 npx dotenv release-it
+git checkout develop
+git merge master
